ca1cc8c46d24e5ee3ff5d984a6fd7cab80d66e34
Nereplameri <alperenavci2002@gmail.com>
2022-12-02 12:11:01 +0300

    Learning da github
    Neco arc

---
+++ SimpleMindExample/src/simplemindexample/SimpleMindExample.java
@@ -0,0 +1,13 @@
+package simplemindexample;
+
+
+
+public class SimpleMindExample {
+ 
+    public static void main(String[] args) {
+        int x = 73;
+        x = x / 10;
+        System.out.println(x);
+    }
+    
+}
